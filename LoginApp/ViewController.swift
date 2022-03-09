1f761fd6b62e314eba724450b8cebcb971e4434d
maximsamus <maximsamus1@gmail.com>
2022-03-09 20:11:31 +0400

    UI

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//    }
+
+
+    
+    @IBAction func remindUsername() {
+        showUsername(title: "OOPS!", message: "Your name is User")
     }
-
-
+    
+    @IBAction func remindPassword() {
+        showPassword(title: "OOPS!", message: "Your password is Password")
+    }
+    
 }
 
+
+extension ViewController {
+    
+    private func showUsername(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    private func showPassword(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+//            self.passwordTextField = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+}
